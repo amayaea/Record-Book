bfef8b4325c250b04cc860604fba0edc93c0009e
Emilio Amaya <emilio.a.amaya@vanderbilt.edu>
2019-08-22 13:45:20 -0500

    fixed navbar

--- client/components/BootstrapNav.js
+++ client/components/BootstrapNav.js
@@ -5,9 +5,8 @@ import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
 import {connect} from 'react-redux'
-import {searchAlbums} from '../store'
+import {searchAlbums, logout} from '../store'
 import {NavLink as Link} from 'react-router-dom'
-import {logout} from '../store'
 
 export class BootstrapNav extends Component {
   constructor() {
@@ -38,50 +37,59 @@ export class BootstrapNav extends Component {
   render() {
     const loggedIn = this.props.loggedIn
     return (
-      <Navbar bg="yellow" variant="primary">
-        <Navbar.Brand href={this.redirect()}>
-          <img
-            alt=""
-            src="/images/logo.png"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />
-          {' Record Book'}
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Form inline>
-          <FormControl
-            type="text"
-            placeholder="Search"
-            className="mr-sm-2"
-            onChange={this.handleChange}
-            onSubmit={this.handleSubmit}
-          />
-          <Link to={`/search/${this.state.search}`}>
-            <Button
-              type="submit"
-              variant="dark"
-              onClick={() => this.props.handleSearch(this.state.search)}
-            >
-              Search
-            </Button>
-          </Link>
-        </Form>
-        {loggedIn ? (
-          <>
-            <Nav.Link href="/discover/for-you">Discover</Nav.Link>
-            <Nav.Link href={`/user/${this.props.user.id}`}>Profile</Nav.Link>
-            {/* <Nav.Link href="/settings">Settings</Nav.Link> */}
-            <Nav.Link href="#" onClick={this.props.handleClick}>
-              Logout
-            </Nav.Link>
-          </>
-        ) : (
-          <Nav.Link href="/login">Login</Nav.Link>
-        )}
+      <>
+        <Navbar bg="yellow" variant="primary">
+          <Navbar.Brand href={this.redirect()}>
+            <img
+              alt=""
+              src="/images/logo.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />
+            {' Record Book'}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Form inline>
+            <FormControl
+              type="text"
+              placeholder="Search"
+              className="mr-sm-2"
+              onChange={this.handleChange}
+              onSubmit={this.handleSubmit}
+            />
+            <Link to={`/search/${this.state.search}`}>
+              <Button
+                type="submit"
+                variant="dark"
+                onClick={() => this.props.handleSearch(this.state.search)}
+              >
+                Search
+              </Button>
+            </Link>
+          </Form>
+
+          <Navbar.Collapse className="justify-content-end">
+            {loggedIn ? (
+              <>
+                <Nav.Item>
+                  <Nav.Link href="/discover/for-you">Discover</Nav.Link>
+                </Nav.Item>
+                <Nav.Link href={`/user/${this.props.user.id}`}>
+                  Profile
+                </Nav.Link>
+                {/* <Nav.Link href="/settings">Settings</Nav.Link> */}
+                <Nav.Link href="#" onClick={this.props.handleClick}>
+                  Logout
+                </Nav.Link>
+              </>
+            ) : (
+              <Nav.Link href="/login">Login</Nav.Link>
+            )}
+          </Navbar.Collapse>
+        </Navbar>
         {/* <Nav.Link to="/signup">Sign Up</Nav.Link> */}
-      </Navbar>
+      </>
     )
   }
 }
